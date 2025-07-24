2d45bc1849739b68269df390a3a415f0fe1a5f93
Adilahmedahmed <adilahmed89317@gmail.com>
2025-07-24 23:02:08 +0530

    Bubble sort program with second sort of array

--- Array_programs.c
+++ Array_programs.c
@@ -1,4 +1,27 @@
 #include<stdio.h>
 int main(){
-    printf("Hello, World!\n");
+    int arr[100], n, i, j, temp;
+    printf("Enter the number of elements in the array: "); 
+    scanf("%d", &n);
+    printf("Enter the elements of the array:\n");
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    // Bubble Sort Algorithm
+    for(i = 0; i < n-1; i++) {
+        for(j = 0; j < n-i-1; j++) {
+            if(arr[j] > arr[j+1]) {
+                // Swap arr[j] and arr[j+1]
+                temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+    printf("Sorted array:\n");
+    for(i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+    return 0;   
 }
